281ff1e2aead64b577ee49cce5f18302d04e6eed
hattima-tim <irfanridwan20@gmail.com>
2024-06-20 20:14:29 +0600

    add route for communicating with ai

---
+++ app.js
@@ -0,0 +1,28 @@
+var createError = require("http-errors");
+var express = require("express");
+var logger = require("morgan");
+const cors = require("cors");
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+const corsOptions = {
+  origin: "https://www.prothomalo.com",
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use("/", indexRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  generationConfig: { responseMimeType: "application/json" },
+});
+
+router.post("/ai", async function (req, res) {
+  const prompt = `Give the bad news headlines which includes story about rape, death, suicide, killing from the following headline list: ${req.body.text}.
+  Give me only the headlines in return. For example if I give the following headlines:
+  ['someone is dead','someone won something']
+  you will give me the following json without giving any other text or anything:
+  ['someone is dead']
+  Follow the above format in json.
+  `;
+
+  try {
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+
+    return res.json(JSON.parse(text));
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;

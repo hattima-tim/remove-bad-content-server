055db6926ff82bd96eb70ecd675fe4f3dd7203d0
hattima-tim <irfanridwan20@gmail.com>
2024-06-29 17:23:47 +0600

    change gemini model and prompt

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,18 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-1.5-pro",
   generationConfig: { responseMimeType: "application/json" },
 });
 
 router.post("/ai", async function (req, res) {
-  const prompt = `Give the news headlines which includes story about rape, death, suicide, killing from the following headline list: ${req.body.text}.
-  Give me only the headlines in return. For example if I give the following headlines:
+  const prompt = `Act as a software which takes in an array of news headlines and returns only the negative news headlines. 
+  Give me all the negative news headlines from the following headline list: ${req.body.text}.
+  For example if I give the following headlines:
   ['someone is dead','someone won something','someone did something good','someone got raped','someone did suicide']
   you will give me the following json without giving any other text or anything:
   ['someone is dead','someone got raped','someone did suicide']
-  Follow the above format in json.
+  Follow the above format in json. If there is no negative news give me an empty array (e.g., []). 
   `;
 
   try {
